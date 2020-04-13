06362b18f78654626b8a28f51356087bf8047979
Yeison Montoya <yeisondiseno@gmail.com>
2020-04-13 09:49:26 -0500

    cambio logo
    Signed-off-by: Yeison Montoya <yeisondiseno@gmail.com>

--- React/navidad-platzi/src/App.js
+++ React/navidad-platzi/src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
+import logoPlatzi from './img/platzi-logo.png';
 import './App.css';
+import {CSSTransitionGroup}  from 'react-transition-group'; // ES6
+
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <CSSTransitionGroup
+          transitionName="fade"
+        >
+          <img src={logoPlatzi} className="App-logo" alt="logo" />
+        </CSSTransitionGroup>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
